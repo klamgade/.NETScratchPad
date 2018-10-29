223a81d5e9dedcb4d2f4bab222f2eb7b9c27aa56
kamal lamgade <kamal.lamgade@outlook.com>
2018-10-29 22:48:21 +1300

    basic crud functionality implementation

--- DotNetCrud/Controllers/ItemController.cs
+++ DotNetCrud/Controllers/ItemController.cs
@@ -1,7 +1,10 @@
 ﻿using DotNetCrud.Models;
+using DotNetCrud.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,23 +12,84 @@ namespace DotNetCrud.Controllers
 {
     public class ItemController : Controller
     {
-        DatabaseContext dbContext = new DatabaseContext();
+        public DatabaseContext _dbContext = new DatabaseContext();
 
-    //POST: Add Item
-        public ActionResult AddItems()
+        public ItemController()
         {
-            using(dbContext)
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            using(_dbContext)
             {
-                var item = new Item()
-                {
-                    Name = "Cheese"
-                };
-                dbContext.Items.Add(item);
-                dbContext.SaveChanges();
+                _dbContext.Items.Add(item);
+                _dbContext.SaveChanges();
             }
             return View();
         }
 
-        private 
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            using (_dbContext)
+            {
+                var items =  _dbContext.Items.ToListAsync();
+                return View(await items);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetItem(int id)
+        {
+            using (_dbContext)
+            {
+                var item =  _dbContext.Items.Single(x => x.Id == id);
+                return View(item);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            using (_dbContext)
+            {
+                var item = _dbContext.Items.Single(x => x.Id == id);
+                return View(item);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Item item)
+        {
+            if(ModelState.IsValid)
+            {
+                using (_dbContext)
+                {
+                    _dbContext.Entry(item).State = EntityState.Modified;
+                    _dbContext.SaveChanges();
+                    return RedirectToAction("index", new { id = item.Id });
+                }
+            }
+            return View(item);
+        }
+
+  
+        public ActionResult Delete(int id)
+        {
+            using (_dbContext)
+            {
+                var item = _dbContext.Items.Single(x => x.Id == id);
+                _dbContext.Items.Remove(item);
+                _dbContext.SaveChanges();
+                return RedirectToAction("index");
+            }
+        }
     }
 }
